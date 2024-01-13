95b9cd51096806f6edd14a1f37d80d118caad095
LahoucineChhaibi <Lahoucine.chhaibi@gmail.com>
2024-01-13 14:45:12 -0800

    Updated

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Script that lists all states from the database hbtn_0e_0_usa"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    us = sys.argv[1]
+    pas = sys.argv[2]
+    dbn = sys.argv[3]
+    h = "localhost"
+    db = MySQLdb.connect(host=h, user=us, passwd=pas, db=dbn, port=3306)
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
